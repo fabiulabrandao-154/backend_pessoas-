5cacfcc1ced84054c9c7576f6c670c89a8c3fb42
Fabíula Brandão <fabiula.brandao15@gmail.com>
2025-12-18 18:28:19 -0300

    Update connect.js

--- src/database/connect.js
+++ src/database/connect.js
@@ -6,10 +6,11 @@ dotenv.config();
 export async function connectDB() {
   try {
     console.log('🔍 URI do Mongo:', process.env.MONGO_URI=mongodb+srv://titafabiula1_db_user:<evpDR2UhR36GPeWo>@cluster0.bdvr7cd.mongodb.net/?appName=Cluster0);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI=mongodb+srv://titafabiula1_db_user:<evpDR2UhR36GPeWo>@cluster0.bdvr7cd.mongodb.net/?appName=Cluster0);
     console.log('✅ MongoDB conectado com sucesso!');
   } catch (err) {
     console.error('❌ Erro ao conectar ao MongoDB:', err.message);
   }
 }
+
 
